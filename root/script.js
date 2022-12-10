bdb80d97b54c9b18e19f5baccea60bc5c4e9feab
mg751 <mgoreansky@gmail.com>
2022-12-10 12:15:31 +0200

    Add player name and lobby form

--- root/script.js
+++ root/script.js
@@ -1,13 +1,31 @@
-const stepOneBtn = document.getElementById("steponebtn");
-const stepOneForm = document.getElementById("continentform")
+const lobbyForm = document.getElementById("lobbyform")
+const lobbyFormBtn = document.getElementById("lobbyformbtn")
+const continentForm = document.getElementById("continentform")
+const continentFormBtn = document.getElementById("continentformbtn")
+const countryForm = document.getElementById("countryform")
+const lobbyCode = document.getElementById("lobbycode");
+const playerName = document.getElementById("playername");
 
-stepOneBtn.addEventListener('click', finishStepOne);
+lobbyFormBtn.addEventListener('click', chooseLobby);
+continentFormBtn.addEventListener('click', chooseContinent);
+
+lobbyForm.addEventListener('input', function() {
+    if (lobbyCode.value.length > 0 && playerName.value.length > 0) {
+        lobbyFormBtn.removeAttribute('disabled');
+    } else {
+        lobbyFormBtn.setAttribute('disabled', '');
+    }
+});
 
 
-function finishStepOne() {
-    stepOneForm.classList.add("hidden");
+function chooseLobby() {
+    lobbyForm.classList.add("hidden");
+    continentForm.classList.remove("hidden");
 }
 
 
-
+function chooseContinent() {
+    continentForm.classList.add("hidden");
+    countryForm.classList.remove("hidden");
+}
 
