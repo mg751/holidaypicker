e319391598cb1561c62635c987c9f26b70e0cd9e
mg751 <mgoreansky@gmail.com>
2022-12-10 20:32:17 +0200

    Fix fonts, update form layout

--- root/script.js
+++ root/script.js
@@ -1,16 +1,18 @@
+let playerName;
+let lobbyCode;
 const lobbyForm = document.getElementById("lobbyform")
 const lobbyFormBtn = document.getElementById("lobbyformbtn")
 const continentForm = document.getElementById("continentform")
 const continentFormBtn = document.getElementById("continentformbtn")
 const countryForm = document.getElementById("countryform")
-const lobbyCode = document.getElementById("lobbycode");
-const playerName = document.getElementById("playername");
+const lobbyCodeInput = document.getElementById("lobbycode");
+const playerNameInput = document.getElementById("playername");
 
 lobbyFormBtn.addEventListener('click', chooseLobby);
 continentFormBtn.addEventListener('click', chooseContinent);
 
 lobbyForm.addEventListener('input', function() {
-    if (lobbyCode.value.length > 0 && playerName.value.length > 0) {
+    if (lobbyCodeInput.value.length > 0 && playerNameInput.value.length > 0) {
         lobbyFormBtn.removeAttribute('disabled');
     } else {
         lobbyFormBtn.setAttribute('disabled', '');
@@ -19,6 +21,8 @@ lobbyForm.addEventListener('input', function() {
 
 
 function chooseLobby() {
+    playerName = playerNameInput.value;
+    lobbyCode = lobbyCodeInput.value;
     lobbyForm.classList.add("hidden");
     continentForm.classList.remove("hidden");
 }
